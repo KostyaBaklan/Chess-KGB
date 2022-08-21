c79cc16850d922f15186241b574000ac65f5c2d3
Kostya Baklan <kostya.baklan@philips.com>
2022-08-21 14:16:39 +0300

    Added History Sorter

---
+++ Chess/Engine/DataStructures/Moves/Collections/History/HistoryMoveCollection.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using Engine.Models.Moves;
+using Engine.Sorting.Comparers;
+
+namespace Engine.DataStructures.Moves.Collections.History
+{
+    public abstract class HistoryMoveCollection : AttackCollection
+    {
+        protected readonly MoveList _killers;
+        protected readonly MoveList _nonCaptures;
+
+        protected HistoryMoveCollection(IMoveComparer comparer) : base(comparer)
+        {
+            _killers = new MoveList();
+            _nonCaptures = new MoveList();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddKillerMove(MoveBase move)
+        {
+            _killers.Add(move);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddNonCapture(MoveBase move)
+        {
+            _nonCaptures.Add(move);
+        }
+    }
+}
---
+++ Chess/Engine/Sorting/Sorters/History/HistoryTradeSorter.cs
@@ -0,0 +1,204 @@
+﻿using System.Runtime.CompilerServices;
+using Engine.DataStructures.Moves;
+using Engine.DataStructures.Moves.Collections.History;
+using Engine.Interfaces;
+using Engine.Models.Enums;
+using Engine.Models.Moves;
+using Engine.Sorting.Comparers;
+
+namespace Engine.Sorting.Sorters.History
+{
+    public class HistoryTradeSorter : HistorySorter
+    {
+        public HistoryTradeSorter(IPosition position, IMoveComparer comparer) : base(position, comparer)
+        {
+            HistoryMoveCollection = new HistoryTradeMoveCollection(comparer);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void ProcessMoves(MoveList moves)
+        {
+            if (Position.GetTurn() == Turn.White)
+            {
+                if (Position.CanWhitePromote())
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (CurrentKillers.Contains(move.Key))
+                        {
+                            HistoryMoveCollection.AddKillerMove(move);
+                        }
+                        else if (move.IsPromotion)
+                        {
+                            ProcessPromotion(move, HistoryMoveCollection);
+                        }
+                        else
+                        {
+                            HistoryMoveCollection.AddNonCapture(move);
+                        }
+                    }
+                }
+                else
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (CurrentKillers.Contains(move.Key))
+                        {
+                            HistoryMoveCollection.AddKillerMove(move);
+                        }
+                        else
+                        {
+                            HistoryMoveCollection.AddNonCapture(move);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Position.CanBlackPromote())
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (CurrentKillers.Contains(move.Key))
+                        {
+                            HistoryMoveCollection.AddKillerMove(move);
+                        }
+                        else if (move.IsPromotion)
+                        {
+                            ProcessPromotion(move, HistoryMoveCollection);
+                        }
+                        else
+                        {
+                            HistoryMoveCollection.AddNonCapture(move);
+                        }
+                    }
+                }
+                else
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (CurrentKillers.Contains(move.Key))
+                        {
+                            HistoryMoveCollection.AddKillerMove(move);
+                        }
+                        else
+                        {
+                            HistoryMoveCollection.AddNonCapture(move);
+                        }
+                    }
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void ProcessMoves(MoveList moves, MoveBase pvNode)
+        {
+            if (Position.GetTurn() == Turn.White)
+            {
+                if (Position.CanWhitePromote())
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (move.Key == pvNode.Key)
+                        {
+                            HistoryMoveCollection.AddHashMove(move);
+                        }
+                        else
+                        {
+                            if (CurrentKillers.Contains(move.Key))
+                            {
+                                HistoryMoveCollection.AddKillerMove(move);
+                            }
+                            else if (move.IsPromotion)
+                            {
+                                ProcessPromotion(move, HistoryMoveCollection);
+                            }
+                            else
+                            {
+                                HistoryMoveCollection.AddNonCapture(move);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (move.Key == pvNode.Key)
+                        {
+                            HistoryMoveCollection.AddHashMove(move);
+                        }
+                        else
+                        {
+                            if (CurrentKillers.Contains(move.Key))
+                            {
+                                HistoryMoveCollection.AddKillerMove(move);
+                            }
+                            else
+                            {
+                                HistoryMoveCollection.AddNonCapture(move);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Position.CanBlackPromote())
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (move.Key == pvNode.Key)
+                        {
+                            HistoryMoveCollection.AddHashMove(move);
+                        }
+                        else
+                        {
+                            if (CurrentKillers.Contains(move.Key))
+                            {
+                                HistoryMoveCollection.AddKillerMove(move);
+                            }
+                            else if (move.IsPromotion)
+                            {
+                                ProcessPromotion(move, HistoryMoveCollection);
+                            }
+                            else
+                            {
+                                HistoryMoveCollection.AddNonCapture(move);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (var index = 0; index < moves.Count; index++)
+                    {
+                        var move = moves[index];
+                        if (move.Key == pvNode.Key)
+                        {
+                            HistoryMoveCollection.AddHashMove(move);
+                        }
+                        else
+                        {
+                            if (CurrentKillers.Contains(move.Key))
+                            {
+                                HistoryMoveCollection.AddKillerMove(move);
+                            }
+                            else
+                            {
+                                HistoryMoveCollection.AddNonCapture(move);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
